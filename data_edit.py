4f531d8cb43625c380fdc8c29598514e853383d0
RimRim <rimrimrimrim1987@gmail.com>
2025-12-28 19:13:17 +0800

    update data_edit.py

--- data_edit.py
+++ data_edit.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
-from typing import Callable, Literal
+from typing import Callable, Literal, TypedDict
 import re
 from re import Pattern
 
@@ -9,13 +9,32 @@ from data_types import AbilityItem, Operation, Weapon
 
 AbilityCategory = Literal["normals", "dodges", "skills", "discharges", "passives"]
 
+PREVIOUS: Literal["PREVIOUS"] = "PREVIOUS"
 
-class PostFormat(ABC):
+
+class TextFormat(ABC):
+    """
+    A text formatting added to an input text.
+    Can be used as 'Modification', in which case, it is applied to the Ability description.
+    """
+
     @abstractmethod
     def __call__(self, text: str) -> str: ...
 
 
-class Remove(PostFormat):
+class Modification(ABC):
+    "A more complicated modification"
+
+    @abstractmethod
+    def __call__(
+        self,
+        weapon: Weapon,
+        ability: AbilityItem,
+        from_category: AbilityCategory,
+    ): ...
+
+
+class Remove(TextFormat):
     "Removes the specified text or regex"
 
     def __init__(self, text_or_pattern: str | Pattern) -> None:
@@ -25,7 +44,7 @@ class Remove(PostFormat):
         if isinstance(self.text_or_pattern, str):
             new_text = text.replace(self.text_or_pattern, "")
         else:
-            new_text = re.sub(self.text_or_pattern, "", text, flags=re.DOTALL)
+            new_text = re.sub(self.text_or_pattern.pattern, "", text, flags=re.DOTALL)
 
         if text == new_text:
             raise ValueError(f"Failed to remove '{self.text_or_pattern}'")
@@ -33,7 +52,14 @@ class Remove(PostFormat):
         return new_text
 
 
-class Strip(PostFormat):
+class InsertNewlines(TextFormat):
+    "Inserts newlines before each matched regex"
+
+    def __init__(self, regex: str | Pattern) -> None:
+        self.regex = regex
+
+
+class Strip(TextFormat):
     "Strips leading and trailing whitespaces"
 
     def __init__(self) -> None:
@@ -41,10 +67,6 @@ class Strip(PostFormat):
 
     def __call__(self, text: str) -> str:
         return text.strip()
-
-
-class Modification(ABC):
-    pass
 
 
 @dataclass
@@ -110,57 +132,150 @@ class Move(Modification):
         ability.desc = re.sub(self.regex.pattern, "", ability.desc, flags=re.DOTALL)
 
 
-_mods: dict[str, dict[str, list[Modification]]] = {
+@dataclass
+class Modify(Modification):
+    "Replaces the specified fields in the Ability"
+
+    name: str | None = field(default=None)
+    desc: str | None = field(default=None)
+    icon: str | None = field(default=None)
+    control: list[Operation] | None = field(default=None)
+
+    def __call__(
+        self, weapon: Weapon, ability: AbilityItem, from_category: AbilityCategory
+    ):
+        if self.name is not None:
+            ability.name = self.name
+        if self.desc is not None:
+            ability.desc = self.desc
+        if self.icon is not None:
+            ability.icon = self.icon
+        if self.control is not None:
+            ability.control = self.control
+
+
+#####
+
+WeaponName = str
+AbilityName = str
+AbilityOrMany = AbilityName | Literal["NORMALS", "DODGES", "SKILLS", "DISCHARGES", "*"]
+
+_ModificationDict = dict[
+    WeaponName,
+    dict[
+        AbilityOrMany,
+        TextFormat
+        | Modification
+        | list[Modification | TextFormat]
+        | Literal["PREVIOUS"],
+    ],
+]
+
+MODS: _ModificationDict = {
     "Liu Huo": {
-        "In All Directions": [
-            Move(
-                to="passives",
-                regex=re.compile(r"\r\n\r\n.*"),
-                post_format=[
-                    Remove("\r\n\r\n <shuzhi>Passive: Calligraphy Characters</>\r\n"),
-                    Strip(),
-                ],
-                name="Calligraphy Characters",
-            )
-        ],
-        "A Spark of Genius": [
-            Move(
-                to="passives",
-                regex=re.compile(r"\r\n.*"),
-                name="Fortitude Resonance",
-            )
-        ],
+        "In All Directions": Move(
+            to="passives",
+            regex=re.compile(r"\r\n\r\n.*"),
+            post_format=[
+                Remove("\r\n\r\n <shuzhi>Passive: Calligraphy Characters</>\r\n"),
+                Strip(),
+            ],
+            name="Calligraphy Characters",
+        ),
+        "A Spark of Genius": Move(
+            to="passives",
+            regex=re.compile(r"\r\n.*"),
+            name="Fortitude Resonance",
+        ),
+    },
+    "Ji Yu": {
+        "Shifting Stars": Move(
+            to="passives",
+            regex=re.compile(r"\r\n Grants.*"),
+            name="Sharp Blade",
+        ),
+        "Review Board": Remove(re.compile(r"\r\n Grants.*")),
+        "Starting Move": PREVIOUS,
     },
 }
 
 
-def apply_mod(weapons: list[Weapon], mods: dict[str, dict[str, list[Modification]]]):
-    wpn_d = {wpn.char: wpn for wpn in weapons}
-    wpn_ab_d = {}
+def _apply_mod_single(
+    modi: TextFormat | Modification | list[TextFormat | Modification],
+    weapon: Weapon,
+    ability: AbilityItem,
+    ability_category: AbilityCategory,
+):
+    if not isinstance(modi, list):
+        modi = [modi]
+
+    for _modi in modi:
+        if isinstance(_modi, TextFormat):
+            ability.desc = _modi(ability.desc)
+        elif isinstance(_modi, Modification):
+            _modi(weapon, ability, ability_category)
+        else:
+            raise ValueError(f"Invalid item {_modi}")
+
+
+def _apply_mod_multi(
+    modi: TextFormat | Modification | list[TextFormat | Modification],
+    weapon: Weapon,
+    abilities: list[AbilityItem],
+    ability_categories: AbilityCategory | list[AbilityCategory],
+):
+    if isinstance(ability_categories, str):
+        for ability in abilities:
+            _apply_mod_single(modi, weapon, ability, ability_categories)
+    else:
+        for ability, ability_cat in zip(abilities, ability_categories):
+            _apply_mod_single(modi, weapon, ability, ability_cat)
+
+
+def apply_mod(weapons: list[Weapon], mods: _ModificationDict):
+    weapon_name_d = {wpn.char: wpn for wpn in weapons}
+    weapon_ability_d: dict[str, dict[str, tuple[AbilityItem, AbilityCategory]]] = {}
+
     for wpn in weapons:
-        wpn_ab_d[wpn.char] = {}
+        weapon_ability_d[wpn.char] = {}
         for ab in wpn.normals:
-            wpn_ab_d[wpn.char][ab.name] = {"obj": ab, "from": "normals"}
+            weapon_ability_d[wpn.char][ab.name] = (ab, "normals")
         for ab in wpn.dodges:
-            wpn_ab_d[wpn.char][ab.name] = {"obj": ab, "from": "dodges"}
+            weapon_ability_d[wpn.char][ab.name] = (ab, "dodges")
         for ab in wpn.skills:
-            wpn_ab_d[wpn.char][ab.name] = {"obj": ab, "from": "skills"}
+            weapon_ability_d[wpn.char][ab.name] = (ab, "skills")
         for ab in wpn.discharges:
-            wpn_ab_d[wpn.char][ab.name] = {"obj": ab, "from": "discharges"}
+            weapon_ability_d[wpn.char][ab.name] = (ab, "discharges")
 
     for _wpn_name in mods:
-        weapon = wpn_d[_wpn_name]
+        previous_modi = None
+        weapon = weapon_name_d[_wpn_name]
 
         for _ab_name in mods[_wpn_name]:
-            _adict = wpn_ab_d[_wpn_name][_ab_name]
+            modi = mods[_wpn_name][_ab_name]
+            if modi == PREVIOUS:
+                if previous_modi is None:
+                    raise ValueError(
+                        "Cannot use PREVIOUS when no previously used modification!"
+                    )
+                modi = previous_modi
 
-            ability = _adict["obj"]
-            from_category = _adict["from"]
+            match _ab_name:
+                case "NORMALS":
+                    _apply_mod_multi(modi, weapon, weapon.normals, "normals")
+                case "DODGES":
+                    _apply_mod_multi(modi, weapon, weapon.dodges, "dodges")
+                case "SKILLS":
+                    _apply_mod_multi(modi, weapon, weapon.dodges, "skills")
+                case "DISCHARGES":
+                    _apply_mod_multi(modi, weapon, weapon.dodges, "discharges")
+                case "*":
+                    _ablist, _abcats = zip(*weapon_ability_d[weapon.char].values())
+                    _apply_mod_multi(modi, weapon, _ablist, _abcats)  # type: ignore
+                case _:
+                    (ability, ability_cat) = weapon_ability_d[_wpn_name][_ab_name]
+                    _apply_mod_single(modi, weapon, ability, ability_cat)
 
-            for _mod in mods[_wpn_name][_ab_name]:
-                if isinstance(_mod, Move):
-                    _mod(weapon, ability, from_category)
-                else:
-                    raise ValueError(f"Invalid item {_mod}")
+            previous_modi = modi
 
     print("`apply_mod` Done!")
